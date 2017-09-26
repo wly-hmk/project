3fb0d66f4d42bffa344041abca0427221c6d41ba
Extended Onsite <eo@Extendeds-MacBook-Pro.local>
2017-09-26 12:26:47 -0700

    Indicate accepted data formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_action :restrict_content_type, :except => [:show, :index, :delete]
 
 protected
   def authenticate
@@ -13,6 +14,14 @@ protected
     # Need to clear the caches for all the pages for a site
     site.page.each do |page|
       Rails.cache.delete(site.url + '/' + page.slug)
+    end
+  end
+
+  def restrict_content_type
+    allowed_types = ['application/json', 'multipart/form-data']
+    unless allowed_types.include? request.content_type
+      render json: {message: 'Content-Type must be application/json'},
+             status: 415
     end
   end
 end
